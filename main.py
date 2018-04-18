359fa35c0dc0cbfc96b5f903a31f157e79a1742d
Anndo Ko <handcuffanndo@gmail.com>
2018-04-18 19:53:33 -0400

    Scrape the review page to get: name, ratings

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ text = make_request_using_cache(baseurl)
 soup = BeautifulSoup(text, "html.parser")
 
 # Table 1. Styles: Id, Name
-style_lst = {}
+style_dic = {}
 menu_items = soup.find_all(class_ = "pure-menu-item")
 for item in menu_items:
     link = item.find("a")["href"]
@@ -57,13 +57,58 @@ for style in style_dic:
     style_node = style_dic[style]
 
     # Form the link
-    review_url = baseurl + style_node
+    url = baseurl + style_node
 
     # BeautifulSoup
-    text = make_request_using_cache(review_url)
+    text = make_request_using_cache(url)
     soup = BeautifulSoup(text, "html.parser")
 
     # Get the node & Add to the list
     article_card_items = soup.find_all("h3")
     for item in article_card_items:
         review_node_lst.append(item.parent["href"])
+
+# Go to each review page & scrape
+for node in review_node_lst[:3]:
+    # Form the link
+    url = baseurl + node
+
+    # BeautifulSoup
+    text = make_request_using_cache(url)
+    soup = BeautifulSoup(text, "html.parser")
+
+    # Scrape the page
+
+    # Name
+    name = soup.find("h1").text
+
+    # Rating
+    rating = int(soup.find(class_ = "main-score-overall rating").text[0:2])
+
+    # Table: Aroma, Appearance, Flavor, Mouthfeel
+    table = soup.find_all("tr")
+    table_dic = {}
+    for row in table:
+        # label and value
+        label = row.find_all(class_ = "table-label")[0].text.replace(":", "")
+        value = row.find_all(class_ = "table-label")[1].text[0:2].replace("\n", "")
+
+        if label == "Aroma":
+            table_dic["Aroma"] = value
+        elif label == "Appearance":
+            table_dic["Appearance"] = value
+        elif label == "Flavor":
+            table_dic["Flavor"] = value
+        elif label == "Mouthfeel":
+            table_dic["Mouthfeel"] = value
+
+    # Style
+    # ABV
+    # IBU
+    # BrewerComment
+    # PanelComment
+
+    print(name)
+    print(rating)
+    print(table_dic)
+    print("-"*10)
