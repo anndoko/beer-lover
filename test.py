92570ac97fe657f56d8f1da73ac946f33ab6e357
Anndo Ko <handcuffanndo@gmail.com>
2018-04-20 01:07:08 -0400

    Add TestClasses

--- test.py
+++ test.py
@@ -12,10 +12,56 @@ class TestDatabase(unittest.TestCase):
 # Classes
 class TestClasses(unittest.TestCase):
     def test_style_class(self):
-        pass
+        # Test data
+        test_dic = {
+            'Name': 'test-style',
+            'Node': '/test-node'
+        }
+        # Create Style instance
+        result = Style(test_dic)
+
+        # Check the instance and instance variables/values
+        self.assertIsInstance(result, Style) # check if it's an instance of Style
+        self.assertEqual(result.name, 'test-style')
+        self.assertEqual(result.node, '/test-node')
 
     def test_beer_class(self):
-        pass
+        # Test data
+        test_dic = {
+            'Name': 'test-beer',
+            'Image': 'test-img-link',
+            'Rating': 100,
+            'Aroma': 1,
+            'Appearance': 2,
+            'Flavor': 3,
+            'Mouthfeel': 4,
+            'Style': 'test-style',
+            'StyleDetail': 'test-style-detail',
+            'ABV': 1.0,
+            'IBU': 1.5,
+            'AromaComment': 'test-description',
+            'FlavorComment': 'test-description',
+            'OverallComment': 'test-description',
+        }
+        # Create Beer instance
+        result = Beer(test_dic)
+
+        # Check the instance and instance variables/values
+        self.assertIsInstance(result, Beer) # check if it's an instance of Beer
+        self.assertEqual(result.name, 'test-beer')
+        self.assertEqual(result.image, 'test-img-link')
+        self.assertEqual(result.rating, 100)
+        self.assertEqual(result.aroma, 1)
+        self.assertEqual(result.appearance, 2)
+        self.assertEqual(result.flavor, 3)
+        self.assertEqual(result.mouthfeel, 4)
+        self.assertEqual(result.style, 'test-style')
+        self.assertEqual(result.style_detail, 'test-style-detail')
+        self.assertEqual(result.abv, 1.0)
+        self.assertEqual(result.ibu, 1.5)
+        self.assertEqual(result.aroma_comment, 'test-description')
+        self.assertEqual(result.flavor_comment, 'test-description')
+        self.assertEqual(result.overall_comment, 'test-description')
 
 # Queries
 class TestQueries(unittest.TestCase):
